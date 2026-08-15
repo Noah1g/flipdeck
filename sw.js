bc669b6387bd266b4c9a60c724a7c282c2f4fadf
Noah1g <noahlohmeyer3107@icloud.com>
2026-08-16 00:52:56 +0200

    Bestand: Tags-Feld in Notizen umbenannt (v5.21.11)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Schreibvorgänge werden NICHT abgefangen: ohne Netz schlagen sie fehl,
      statt so zu tun, als wäre gespeichert worden.
 */
-const VERSION    = "flipdeck-v90";   // v90 (App v5.21.10): Amazon und Etsy nicht mehr in Marktplatz-Auswahl (noch nicht ausgereift)
+const VERSION    = "flipdeck-v91";   // v91 (App v5.21.11): Bestand - Tags-Feld in Notizen umbenannt
 const SHELL      = `${VERSION}-shell`;
 const DATA       = `${VERSION}-data`;
 const IMAGES     = `${VERSION}-img`;
